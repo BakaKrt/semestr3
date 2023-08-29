8baf11022ad2d106c69a8229107fa918b12b7715
BakaKrt <eardykov@gmail.com>
2023-08-30 00:22:24 +0300

    upd l1

--- laba1/laba1/laba1.cpp
+++ laba1/laba1/laba1.cpp
@@ -1,9 +1,14 @@
 ﻿#include <iostream>
 
+#define debug(Expression)\
+     std::cout << "\n{" #Expression "}: " << (Expression) << std::endl;
+
+
 int main()
 {
     setlocale(0, "");
     std::cout << "Hello World!\n";
     std::cout << "хуй сперма трахать пирожки";
+
 }
 
