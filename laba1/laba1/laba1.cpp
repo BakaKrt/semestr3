f66a0b07e228fd33fb2aaeb7f9b9470d6e04d07e
BakaKrt <eardykov@gmail.com>
2023-09-04 18:42:50 +0300

    ва

--- laba1/laba1/laba1.cpp
+++ laba1/laba1/laba1.cpp
@@ -13,7 +13,7 @@ int main()
     std::cout << "Hello World!\n";
     vector<int> vec = {1,2,3,4,5,6,7,8,9,10,11,12};
     for (int a = vec[0]; a += vec[a]; a < vec.size()) {
-        printf("%d %d %d\n", a, vec.size(), a<vec.size());
+        printf("%d %d %d\n", a, vec.size(), a < vec.size());
         //debug(a);
         //debug(vec.size());
         if (a > vec.size()) break;
