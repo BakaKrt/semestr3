91ddeed39d0d87434b2681e7e1922b9e08197f3a
BakaKrt <eardykov@gmail.com>
2023-10-07 21:52:58 +0300

    да

--- ООП/laba2/laba2.cpp
+++ ООП/laba2/laba2.cpp
@@ -15,18 +15,18 @@ double factorial(double f) {
 int main() {
 
     setlocale(0, "");
-    long double x1, x2, dx;
-    double eps = (1e-4);
-
+    long double x1, x2, dx; ;
+    double eps = (1e-5); 
+    printf("Введите А и B, с шагом в dx: ");
     cin >> x1 >> x2 >> dx;
 
     cout << left << setw(25) << "    Значение x" << setw(24) << "Прибл-ое зн-ие" << setw(25) << "Настоящее зн-ие" << setw(25) << "Кол-во эл-тов\n";
 
     for (double x = x1; x <= x2; x += dx) {
 
-        long double calc = 0;
-        long double result = 0;
-        int n = 0; bool skip = false;
+        double calc = 0;
+        double result = 0;
+        int n = 0;
 
         do {
             calc += result;
@@ -34,7 +34,8 @@ int main() {
             n++;
         } while (fabs(result) >= eps);
 
-        if (x == x1) cout << "\n";
+        if (x == x1)
+            cout << "\n";
         cout << fixed << "     " << left << setw(23) << x << setw(24) << calc << setw(28) << sin(x)/x << setw(25) << n << endl;
     }
 }
--- ООП/laba3_B/laba3_B.cpp
+++ ООП/laba3_B/laba3_B.cpp
@@ -1,16 +1,87 @@
-﻿#include <iostream>
+﻿#include <iostream>y
+#include <vector>
+#include <random>
+#include <ctime>
+#include <cstdlib>
+
 using namespace std;
+
+void fill_rand(vector<vector<int>> &vec) {
+    srand(time(nullptr));
+    for (auto& y : vec) {
+        for (auto& x : y) {
+            x = rand() % 20;
+        }
+    }
+}
+
+void print_vector(vector<vector<int>>& vec) {
+    for (auto& x : vec) {
+        for (auto& y : x) {
+            cout << y << "\t";
+        }
+        cout << endl;
+    }
+}
+
+void func(vector<vector<int>>& vec) {
+    vector<int> s = vec[0]; int buff = s[s.size()-1];
+    for (int x = 0; x < vec.size(); x++) {
+        if (x == 0) {
+            for (int y = vec[x].size() - 1; y > 0; y--) {
+                vec[x][y] = vec[x][y - 1];
+            }
+        }
+    }
+    s = vec[vec.size() - 1];
+    int buff_ = s[s.size() - 1];
+    for (int x = vec.size()-1; x > 1; x--) {
+        vec[x][vec[x].size() - 1] = vec[x-1][vec[x].size() - 1];
+    }
+    vec[vec[vec.size()-1].size() - vec.size() + 2][vec[vec.size()-1].size() - 1] = buff;
+    
+    buff = buff_;
+
+    print_vector(vec);
+    printf("Это buff: %d\n", buff);
+    vec[vec[vec.size() - 1].size()-2][vec[vec.size() - 1].size()-1] = buff;
+
+    s = vec[vec.size() - 1];
+    buff = s[0];
+
+
+    for (int x = vec.size()-1; x > 0; x--) {
+        for (int y = 1; y < vec[x].size()-1; y++) {
+            vec[x][y-1] = vec[x][y];
+        }
+        break;
+    }
+    vec[vec[vec.size() - 1].size()][vec[vec.size() - 1].size()-2] = buff;
+
+
+    for (int x = 0; x < vec.size()-1; x++) {
+        vec[x][0] = vec[x+1][0];
+    }
+
+    vec[vec[vec.size() - 1].size()-1][0] = buff;
+    }
+
 
 int main()
 {
     setlocale(0, "");
-    int mas[6][5];
-    int K;
-    cout << "Введите число циклов: ";
-    cin >> K;
-    for (int x = 0; x < 6; x++) {
-        for (int y = 0; y < 5; y++) {
-
-        }
+    vector<vector<int>> vec(6, {0,0,0,0,0});
+    
+    fill_rand(vec);
+    printf("Исходная случайная матрица 6*5:\n");
+    print_vector(vec);
+    cout << endl;
+    int kol;
+    cout << "Введите количество перемещений: ";
+    cin >> kol;
+    for (int i = 0; i < kol; i++) {
+        func(vec);
     }
+    cout << endl;
+    print_vector(vec);
 }
