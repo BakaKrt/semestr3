b12db6d1ae4f1770bac0ac06a784ccfc8a7853f6
BakaKrt <eardykov@gmail.com>
2023-10-11 23:25:11 +0300

    да

--- САОД/SAOD_LABA3_3/SAOD_LABA3_3.cpp
+++ САОД/SAOD_LABA3_3/SAOD_LABA3_3.cpp
@@ -1,40 +1,57 @@
 ﻿#include <vector>
-#include <algorithm>
 #include <iostream>
+#include <random>
+#include <ctime>
+#include <cstdlib>
+#include <thread>
+#include <iomanip>
+
 using namespace std;
 
-struct Order {
-    double penalty; // штраф
-    double deadline; // срок выполнения
-};
-
-bool compareOrders(Order a, Order b) {
-    return (a.deadline < b.deadline);
+void get_random_vector(vector<vector<int>> &a) {
+    srand(time(nullptr));
+    for (auto &x : a) {
+        for (auto &y : x) {
+            y = rand() % 100;
+        }
+    }
 }
 
-double calculatePenalty(vector<Order> orders) {
-    sort(orders.begin(), orders.end(), compareOrders);
-
-    double totalPenalty = 0.0;
-    for (int i = 0; i < orders.size(); i++) {
-        totalPenalty += orders[i].penalty;
+void print_vector(vector<vector<int>>& a) {
+    for (auto &x : a) {
+        for (auto &y : x) {
+            cout << setw(3) << y << " ";
+        }
+        cout << endl;
     }
+}
 
-    return totalPenalty;
-
+int len_of_path(vector<vector<int>>& in) {
+    int cur_x = 0, cur_y = 0, in_size = in.size() - 1;
+    int sum = in[cur_y][cur_x];
+    vector<int> path = { in[cur_y][cur_x] };
+    while (cur_x != in_size && cur_y != in_size) {
+        if (in[cur_y][cur_x + 1] < in[cur_y + 1][cur_x]) {
+            sum += in[cur_y][cur_x + 1];
+            path.push_back(in[cur_y][cur_x + 1]);
+            cur_x++;
+        }
+        else {
+            sum += in[cur_y+1][cur_x];
+            path.push_back(in[cur_y + 1][cur_x]);
+            cur_y++;
+        }
+    }
+    for (const auto& p : path) {
+        cout << p << " ";
+    } cout << endl;
+    return sum;
 }
 
 int main() {
     setlocale(0, "");
-    vector<Order> orders;
-
-    Order first;
-    first.deadline = 10; first.penalty = 5;
-    orders.push_back(first);
-    //calculatePenalty(orders);
-
-    cout << "Минимальная сумма штрафов равна: " << calculatePenalty(orders) << endl;
-
-    return 0;
-
+    vector<vector<int>> pole(10, {0,0,0,0,0,0,0,0,0,0});
+    get_random_vector(pole);
+    print_vector(pole);
+    printf("Ответ: %d", len_of_path(pole));
 }
